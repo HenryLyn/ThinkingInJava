1b6baaae31cfccebca6fcdbfdf4d444ae56aefdb
Henry Lin <huarui.lin@galasys.cn>
2018-08-09 11:23:40 +0800

    1. assignment.

---
+++ src/main/java/top/linhuarui/thinking_in_java/chapter3/section3_1_2/Assiginment.java
@@ -0,0 +1,29 @@
+package top.linhuarui.thinking_in_java.chapter3.section3_1_2;
+
+/**
+* @author LHR
+* @date 2018/8/8
+* @desc Assignment with objects is a bit tricky
+* @version 1.0
+*/
+public class Assiginment {
+
+    public static void main (String args[]) {
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 9;
+        n2.i = 47;
+
+        System.out.println("1: n1.i: " + n1.i + ", n2.i: " + n2.i);
+        n1 = n2;
+        System.out.println("2: n1.i: " + n1.i + ", n2.i: " + n2.i);
+        n1.i = 27;
+        System.out.println("3: n1.i: " + n1.i + ", n2.i: " + n2.i);
+
+
+    }
+}
+
+class Number {
+    int i;
+}
---
+++ src/main/java/top/linhuarui/thinking_in_java/chapter3/section3_1_2/PassObject.java
@@ -0,0 +1,25 @@
+package top.linhuarui.thinking_in_java.chapter3.section3_1_2;
+/**
+* @author LHR
+* @date 2018/8/8
+* @desc  Passing objects to methods can be a bit tricky
+* @version 1.0
+*/
+public class PassObject {
+
+    public static void main (String args[]) {
+        Letter x = new Letter();
+        x.c = 'a';
+        System.out.println("1: x.c: " + x.c);
+        f(x);
+        System.out.println("2: x.c: " + x.c);
+    }
+
+    static void f(Letter y) {
+        y.c = 'z';
+    }
+}
+
+class Letter {
+    char c;
+}
