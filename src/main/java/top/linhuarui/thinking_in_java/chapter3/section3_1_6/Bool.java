eaf31eece5b64f73cd62cbc981ed40fdf580df5e
Henry Lin <huarui.lin@galasys.cn>
2018-08-09 17:22:01 +0800

    1. Logical operator practice.

---
+++ src/main/java/top/linhuarui/thinking_in_java/chapter3/section3_1_6/Bool.java
@@ -0,0 +1,36 @@
+package top.linhuarui.thinking_in_java.chapter3.section3_1_6;
+
+import java.util.Random;
+
+/**
+* @author LHR
+* @date 2018/8/9
+* @desc Relational and logical operators
+* @version 1.0
+*/
+public class Bool {
+
+    public static void main (String[] args) {
+        Random rand = new Random();
+        int i = rand.nextInt() % 100;
+        int j = rand.nextInt() % 100;
+        prt("i = " + i);
+        prt("j = " + j);
+        prt("i > j is " + (i > j));
+        prt("i < j is " + (i < j));
+        prt("i >= j is " + (i >= j));
+        prt("i <= j is " + (i <= j));
+        prt("i == j is " + (i == j));
+        prt("i != j is " + (i != j));
+
+        // Treating an int as a boolean is not legal Java
+        //! prt("i && j is " + (i && j));
+
+        prt("(i < 10) && (j < 10) is " + ((i < 10) && (j < 10)));
+        prt("(i < 10) || (j < 10) is " + ((i < 10) || (j < 10)));
+    }
+
+    static void prt (String s) {
+        System.out.println(s);
+    }
+}
