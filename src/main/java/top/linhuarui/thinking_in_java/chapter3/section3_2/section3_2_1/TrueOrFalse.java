cef0b287a2af4ed7a1124b27e44e63c3e3374f2f
Henry Lin <huarui.lin@galasys.cn>
2018-08-15 10:11:58 +0800

    1. true or false conditional statement.

---
+++ src/main/java/top/linhuarui/thinking_in_java/chapter3/section3_2/section3_2_1/TrueOrFalse.java
@@ -0,0 +1,23 @@
+package top.linhuarui.thinking_in_java.chapter3.section3_2.section3_2_1;
+
+/**
+* @author LHR
+* @date 2018/8/15
+* @desc true or false conditional statement.
+* @version 1.0
+* @modify
+*   @date
+*/
+public class TrueOrFalse {
+
+    public static void main (String[] args) {
+        int a = 0;
+        prt("a == 0 ? " + (a == 0));
+        prt("a != 0 ? " + (a != 0));
+    }
+
+    private static void prt(String s) {
+        System.out.println(s);
+    }
+
+}
