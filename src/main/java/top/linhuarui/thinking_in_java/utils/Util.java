26cf8a96003bdfc4252121977f34af88b22056bd
Henry Lin <huarui.lin@galasys.cn>
2018-08-15 13:58:57 +0800

    1. add util package.

---
+++ src/main/java/top/linhuarui/thinking_in_java/utils/Util.java
@@ -0,0 +1,15 @@
+package top.linhuarui.thinking_in_java.utils;
+
+/**
+* @author LHR
+* @date 2018/8/15
+* @desc utils in thinking in java.
+* @version 1.0
+* @modify
+*   @date
+*/
+public class Util {
+    public static void prt(String s) {
+        System.out.println(s);
+    }
+}
