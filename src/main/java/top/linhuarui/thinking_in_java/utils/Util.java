4e6d153303b8edcd4513cbe2f0830d8920c031c4
Henry Lin <huarui.lin@galasys.cn>
2018-08-21 17:53:18 +0800

    1. add new method for get random int value.

--- src/main/java/top/linhuarui/thinking_in_java/utils/Util.java
+++ src/main/java/top/linhuarui/thinking_in_java/utils/Util.java
@@ -8,13 +8,34 @@ import java.util.Random;
 * @desc utils in thinking in java.
 * @version 1.0
 * @modify
-*   @date
+*   @date 2018/08/21 add new method for get random int value
 */
 public class Util {
+    
+    /**
+    * @author LHR
+    * @date 2018/8/21
+    * @desc print string
+    * @params [s]
+    * @returns void
+    * @version 1.0
+    * @modify 
+    *   @date 
+    */
     public static void prt(String s) {
         System.out.println(s);
     }
 
+    /**
+    * @author LHR
+    * @date 2018/8/21
+    * @desc get random int value.
+    * @params [mod]
+    * @returns int
+    * @version 1.0
+    * @modify
+    *   @date
+    */
     public static int randInt (int mod) {
         return Math.abs(new Random().nextInt()) % mod + 1;
     }
