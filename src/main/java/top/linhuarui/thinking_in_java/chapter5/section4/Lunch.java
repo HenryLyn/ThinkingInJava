95356d60b254364ef18a95329c7b88b6db3da243
Henry Lin <huarui.lin@galasys.cn>
2018-08-22 15:35:22 +0800

    1. access specifier exercises..

---
+++ src/main/java/top/linhuarui/thinking_in_java/chapter5/section4/Lunch.java
@@ -0,0 +1,45 @@
+package top.linhuarui.thinking_in_java.chapter5.section4;
+
+/**
+* @author LHR
+* @date 2018/8/22
+* @desc Demonstrates class access specifiers.
+ * Make a class effectively private with private constructors.
+* @version 1.0
+* @modify
+*   @date
+*/
+// Only one public class allowed per file:
+public class Lunch {
+    void test () {
+        // Can't do this! Private constructor:
+        //! Soup priv1 = new Soup();
+        Soup priv2 = Soup.makeSoup();
+        Sandwich f1 = new Sandwich();
+        Soup.access().f();
+    }
+}
+
+class Soup {
+    private Soup() {}
+
+    // (1) Allow creation via static method;
+    public static Soup makeSoup () {
+        return new Soup();
+    }
+
+    // (2) Create a static object and return a reference upon request.
+    // (The "Singleton" pattern):
+    private static Soup ps1 = new Soup();
+    public static Soup access () {
+        return ps1;
+    }
+
+    public void f () {}
+}
+
+class Sandwich { // uses Lunch
+    void f() {
+        new Lunch();
+    }
+}
