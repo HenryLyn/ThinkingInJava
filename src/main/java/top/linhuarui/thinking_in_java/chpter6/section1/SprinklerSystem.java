77ad25921904f5f7ed65c42bdfa6fe329e47f1ea
Henry Lin <huarui.lin@galasys.cn>
2018-08-22 15:59:59 +0800

    1. composition.

---
+++ src/main/java/top/linhuarui/thinking_in_java/chpter6/section1/SprinklerSystem.java
@@ -0,0 +1,44 @@
+package top.linhuarui.thinking_in_java.chpter6.section1;
+
+/**
+* @author LHR
+* @date 2018/8/22
+* @desc Composition for code reuse
+* @version 1.0
+* @modify
+*   @date
+*/
+public class SprinklerSystem {
+    private String value1, value2, value3, value4;
+    WaterSource source;
+    int i;
+    float f;
+
+    void print () {
+        System.out.println("value1 = " + value1);
+        System.out.println("value2 = " + value2);
+        System.out.println("value3 = " + value3);
+        System.out.println("value4 = " + value4);
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("source = " + source);
+    }
+
+    public static void main (String[] args) {
+        SprinklerSystem x = new SprinklerSystem();
+        x.print();
+    }
+}
+
+class WaterSource {
+    private String s;
+    WaterSource () {
+        System.out.println("WaterSource()");
+        s = new String ("Constructed");
+    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
+}
