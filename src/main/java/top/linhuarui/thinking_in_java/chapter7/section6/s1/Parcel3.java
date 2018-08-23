c3afdc6f39715b9e583e408208982d9b62cfaba1
Henry Lin <huarui.lin@galasys.cn>
2018-08-23 16:46:02 +0800

    1. inner classes and upcasting.

---
+++ src/main/java/top/linhuarui/thinking_in_java/chapter7/section6/s1/Parcel3.java
@@ -0,0 +1,60 @@
+package top.linhuarui.thinking_in_java.chapter7.section6.s1;
+
+/**
+* @author LHR
+* @date 2018/8/23
+* @desc Returning a handle to an inner class
+* @version 1.0
+* @modify
+*   @date
+*/
+public class Parcel3 {
+    private class PContents extends Contents {
+        private int i = 11;
+
+        @Override
+        public int value() {
+            return i;
+        }
+    }
+
+    protected class PDestination implements Destination {
+        private String label;
+
+        public PDestination (String whereTo) {
+            label = whereTo;
+        }
+
+        @Override
+        public String readLabel() {
+            return label;
+        }
+    }
+
+    public Destination dest (String s) {
+        return new PDestination(s);
+    }
+
+    public Contents cont () {
+        return new PContents();
+    }
+}
+
+class Test {
+    public static void main (String[] args) {
+        Parcel3 p = new Parcel3();
+        Contents c = p.cont();
+        Destination d = p.dest("Tanzania");
+        // Illegal -- Can't access private class:
+        //! Parcel3.PContents c = new p.PContents();
+    }
+}
+
+abstract class Contents {
+    abstract public int value ();
+}
+
+interface Destination {
+    String readLabel ();
+}
+
